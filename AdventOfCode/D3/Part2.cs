df1cd2ef444849f5c37c3ad9bbbcebc9e1c58d9c
Christian Sindberg <chrsin@gmail.com>
2022-12-10 19:54:06 +0100

    Added Day3 part 2

---
+++ AdventOfCode/D3/Part2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.D3; 
+
+public class Part2 {
+
+	public static int CalculateBadgeSum(string filename) {
+		var input = File.ReadAllText(filename);
+
+		var backpacks = input.Split("\n");
+
+		var backpackSum = 0;
+
+		for (int index = 0; index < backpacks.Length; index = index + 3) {
+			backpackSum += GetBadgePriority(backpacks[index], backpacks[index + 1], backpacks[index + 2]);
+		}
+
+		return backpackSum;
+	}
+
+	private static int GetBadgePriority(string backpack1, string backpack2, string backpack3) {
+
+		foreach (var c in backpack1) {
+			if(backpack2.IndexOf(c) == -1)
+				continue;
+			
+			if(backpack3.IndexOf(c) == -1)
+				continue;
+
+			return GetPriority(c);
+		}
+
+		throw new NotImplementedException("This should not happen");
+	}
+
+	public static int GetPriority(char c) {
+		var cv = (int)c;
+		return cv switch {
+			>= 97 => cv - 96,
+			_ =>  cv - 64 + 26
+		};
+	}
+}
---
+++ AdventOfCode/D3/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.D3; 
+
+public class Tests {
+
+	[Fact]
+	public async Task TestAnswer() {
+		var sum = Part2.CalculateBadgeSum("D3/input3.txt");
+		Assert.Equal(1, sum);
+	}
+
+	[Fact]
+	public void Test1() {
+		var sum = Part2.CalculateBadgeSum("D3/example3.txt");
+		Assert.Equal(70, sum);
+	}
+
+	[Theory]
+	[InlineData('r', 18)]
+	[InlineData('Z', 52)]
+	public async Task TestPriority(char input, int expectedOutput) {
+		Assert.Equal(expectedOutput, Part2.GetPriority(input));
+	}
+
+}
