e77a91b7e5f267988bb87994ddc2857601e50de1
Christian Sindberg <chrsin@gmail.com>
2022-12-11 14:59:44 +0100

    Added Day 6

---
+++ AdventOfCode/D6/Part1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AdventOfCode.D6; 
+
+public class Part1 {
+	public static int FindMessageMarker(string input, int markerLength) {
+		var lastMessages = ImmutableQueue.Create<char>();
+
+		for (int index = 0; index < input.Length; index++) {
+			char c = input[index];
+			if (lastMessages.Count() == markerLength)
+				lastMessages = lastMessages.Dequeue();
+
+			lastMessages = lastMessages.Enqueue(c);
+			bool found = HasDuplicates(lastMessages);
+
+			if (!found && lastMessages.Count() == markerLength)
+				return index + 1;
+		}
+
+		return -1;
+	}
+
+	public static bool HasDuplicates(ImmutableQueue<char> queue) {
+		while (queue.Count() != 0) {
+			queue = queue.Dequeue(out char c);
+			bool found = queue.Contains(c);
+			if (found)
+				return true;
+		}
+
+		return false;
+	}
+}
---
+++ AdventOfCode/D6/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.D6;
+
+public class Tests {
+	[Theory]
+	[InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+	[InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+	[InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+	[InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+	public async Task CanParseExamples(string input, int expectedIndex) {
+		Assert.Equal(expectedIndex, Part1.FindMessageMarker(input, 4));
+	}
+
+	[Fact]
+	public async Task CanParseInputPart1() {
+		Assert.Equal(1282, Part1.FindMessageMarker(File.ReadAllText("D6/input.txt"), 4));
+	}
+
+	[Fact]
+	public async Task CanParseInputPart2() {
+		Assert.Equal(3513, Part1.FindMessageMarker(File.ReadAllText("D6/input.txt"), 14));
+	}
+}
