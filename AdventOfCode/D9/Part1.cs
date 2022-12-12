738794901ec86e1a82a4cfdbaec80f34a5c28de5
Christian Sindberg <chrsin@gmail.com>
2022-12-12 21:22:32 +0100

    Implemented Day9 part1

---
+++ AdventOfCode/D9/Part1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AdventOfCode.D9;
+
+public class Part1 {
+	public const int BoardSize = 10000;
+	public static int[,] InitializeBoard() {
+		var board = new int[BoardSize, BoardSize];
+
+		for (int y = 0; y < board.GetLength(1); y++) {
+			for (int x = 0; x < board.GetLength(0); x++) {
+				board[x, y] = 0;
+			}
+		}
+
+		return board;
+	}
+
+	public static int[,] ApplyMoves(int[,] board, string filename) {
+		int startX = BoardSize / 2;
+		int startY = BoardSize / 2 - 1;
+
+		var lines = File.ReadAllLines(filename);
+
+		Rope rope = new Rope(startX, startY);
+		foreach (var line in lines) {
+			var parts = line.Split(' ');
+			int count = int.Parse(parts[1]);
+			Direction direction = parts[0] switch {
+				"R" => Direction.Right,
+				"U" => Direction.Up,
+				"L" => Direction.Left,
+				"D" => Direction.Down
+			};
+
+			for (int c = 0; c < count; c++) {
+				rope.Move(direction);
+
+				int currentValue = board[rope.TailX, rope.TailY];
+				board[rope.TailX, rope.TailY] = currentValue + 1;
+			}
+		}
+
+		return board;
+	}
+	
+	
+
+	public static int GetNumberOfTailVisitedFields(int[,] board) {
+		int sum = 0;
+		foreach (int value in board) {
+			if (value > 0)
+				sum++;
+		}
+
+		return sum;
+	}
+}
+
+public enum Direction {
+	Up,
+	Down,
+	Left,
+	Right
+}
+
+public class Rope {
+	public Rope(int x, int y) {
+		HeadX = x;
+		TailX = x;
+		HeadY = y;
+		TailY = y;
+	}
+
+	public void Move(Direction direction) {
+		switch (direction) {
+			case Direction.Up:
+				HeadY--;
+				break;
+			case Direction.Down:
+				HeadY++;
+				break;
+			case Direction.Left:
+				HeadX--;
+				break;
+			case Direction.Right:
+				HeadX++;
+				break;
+		}
+
+		if (!IsTailAdjacent()) {
+			MoveTailBehind(direction);
+		}
+	}
+
+	public bool IsTailAdjacent() {
+		return Math.Abs(HeadX - TailX) <= 1 && Math.Abs(HeadY - TailY) <= 1;
+	}
+
+	public void MoveTailBehind(Direction movingDirection) {
+		switch (movingDirection) {
+			case Direction.Up:
+				TailX = HeadX;
+				TailY = HeadY + 1;
+				break;
+			case Direction.Down:
+				TailX = HeadX;
+				TailY = HeadY - 1;
+				break;
+			case Direction.Left:
+				TailX = HeadX + 1;
+				TailY = HeadY;
+				break;
+			case Direction.Right:
+				TailX = HeadX - 1;
+				TailY = HeadY;
+				break;
+		}
+	}
+
+	public int HeadX { get; set; }
+	public int HeadY { get; set; }
+
+	public int TailX { get; set; }
+	public int TailY { get; set; }
+}
---
+++ AdventOfCode/D9/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace AdventOfCode.D9; 
+
+public class Tests {
+
+	[Fact]
+	public async Task CanSolveExamplePart1() {
+		int[,] board = Part1.InitializeBoard();
+
+		board = Part1.ApplyMoves(board, "D9/example.txt");
+
+		int visitedFields = Part1.GetNumberOfTailVisitedFields(board);
+		Assert.Equal(13, visitedFields);
+	}
+	
+	[Fact]
+	public async Task CanSolveInputPart1() {
+		int[,] board = Part1.InitializeBoard();
+
+		board = Part1.ApplyMoves(board, "D9/input.txt");
+
+		int visitedFields = Part1.GetNumberOfTailVisitedFields(board);
+		Assert.Equal(5779, visitedFields);
+	}
+}
